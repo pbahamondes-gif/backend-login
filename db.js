48f1fe3eae7ebb11a2d141e167c1ecd136cbbb27
pbahamondes-gif <pbahamondes@apprecio.com>
2026-04-02 16:56:46 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,11 @@
+import mysql from "mysql2/promise";
 
+export const pool = mysql.createPool({
+  host: "www.help-now.cl",
+  user: "helpnowc_paulina",
+  password: "Paulina2026$",
+  database: "cargaLogosCredenciales",
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
